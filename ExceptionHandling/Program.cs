1241c09963c8e74a9d6241eae4bcf7f34b09fea0
Sahil Mohite <sahilmohite169@gmal.com>
2025-06-06 15:59:44 +0530

    first commit

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first Number");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second Nunber");
+            int b =int .Parse(Console.ReadLine());
+
+            try {
+
+                int result = a / b;
+                Console.Write("The result is: " + result);
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("You cannot divde by zero");
+            }
+
+            Console.WriteLine();
+
+
+            Console.WriteLine("Line 1");
+            Console.WriteLine("Line 2");
+            Console.WriteLine("Line 3");
+        }
+    }
+}
